3879169a5d148a5ea2f938b21871967eefa1c5ca
Matheus Ferreira Riguette de Souza <matheusfriguette@facom.local>
2019-09-10 18:49:44 -0300

    adicionando barra lateral responsiva

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
-let loginForm = document.forms.login;
-loginForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  if (loginForm.checkValidity() === false) {
-    loginForm.classList.add('was-validated');
-  }
+// let loginForm = document.forms.login;
+// loginForm.addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   if (loginForm.checkValidity() === false) {
+//     loginForm.classList.add('was-validated');
+//   }
+// });
+
+$(document).ready(function () {
+  $('#sidebarToggle').on('click', function () {
+      $('.page-sidebar').toggleClass('active');
+  });
 });
