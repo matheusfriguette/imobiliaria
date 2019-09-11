72b475d22ea8aa32df6b418bfc078610f3ef4f6a
Matheus Riguette <matheus.f.riguette@gmail.com>
2019-09-11 14:12:20 -0300

    adding forms

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,22 @@
 //   }
 // });
 
-$(document).ready(function () {
-  $('.sidebar-toggle').on('click', function () {
-      $('.page-sidebar').toggleClass('active');
+$(document).ready(function() {
+  $('.sidebar-toggle').on('click', function() {
+    $('.page-sidebar').toggleClass('active');
+  });
+
+  $('#customFile').on('change', (event) => {
+    let fileReader = new FileReader();
+
+    fileReader.onload = function(e) {
+      let wrapper = $('<div class="col-md-4 py-2"></div>');
+      let img = $('<img class="gallery-image">');
+      img.attr('src', fileReader.result);
+      img.appendTo(wrapper);
+      wrapper.appendTo('#housePictures');
+    };
+
+    fileReader.readAsDataURL(event.target.files[0]);
   });
 });
