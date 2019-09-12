5732ad52a1af2579c13be8c5b1ef7a2c2711e3f5
Matheus Riguette <matheus.f.riguette@gmail.com>
2019-09-12 16:43:04 -0300

    arrumando js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,99 +12,105 @@ $(document).ready(function() {
     $('.page-sidebar').toggleClass('active');
   });
 
-  // $(document.forms.funcionario.cep).on('focusin', (event) => {
-  //   $('#cepInvalido').removeClass('d-block');
-  //   $('#cepInvalido').addClass('d-none');
-  // });
+  if (document.forms.funcionario !== undefined) {
+    $(document.forms.funcionario.cep).on('focusin', (event) => {
+      $('#cepInvalido').removeClass('d-block');
+      $('#cepInvalido').addClass('d-none');
+    });
 
-  // $(document.forms.funcionario.cep).on('focusout', (event) => {
-  //   if (event.target.validity.valid) {
-  //     $.getJSON('https://viacep.com.br/ws/' + document.forms.funcionario.cep.value + '/json/?callback=?', (data) => {
-  //       console.log(data);
-  //       if (data.erro === undefined) {
-  //         $(document.forms.funcionario.estado).val(data.uf);
-  //         $(document.forms.funcionario.cidade).val(data.localidade);
-  //         $(document.forms.funcionario.bairro).val(data.bairro);
-  //         $(document.forms.funcionario.rua).val(data.logradouro);
-  //       } else {
-  //         $('#cepInvalido').toggleClass('d-block');
-  //         $('#cepInvalido').toggleClass('d-none');
-  //       }
-  //     });
-  //   }
-  // });
+    $(document.forms.funcionario.cep).on('focusout', (event) => {
+      if (event.target.validity.valid) {
+        $.getJSON('https://viacep.com.br/ws/' + document.forms.funcionario.cep.value + '/json/?callback=?', (data) => {
+          console.log(data);
+          if (data.erro === undefined) {
+            $(document.forms.funcionario.estado).val(data.uf);
+            $(document.forms.funcionario.cidade).val(data.localidade);
+            $(document.forms.funcionario.bairro).val(data.bairro);
+            $(document.forms.funcionario.rua).val(data.logradouro);
+          } else {
+            $('#cepInvalido').toggleClass('d-block');
+            $('#cepInvalido').toggleClass('d-none');
+          }
+        });
+      }
+    });
+  }
 
-  // $(document.forms.cliente.cep).on('focusin', (event) => {
-  //   $('#cepInvalido').removeClass('d-block');
-  //   $('#cepInvalido').addClass('d-none');
-  // });
+  if (document.forms.cliente !== undefined) {
+    $(document.forms.cliente.cep).on('focusin', (event) => {
+      $('#cepInvalido').removeClass('d-block');
+      $('#cepInvalido').addClass('d-none');
+    });
 
-  // $(document.forms.cliente.cep).on('focusout', (event) => {
-  //   if (event.target.validity.valid) {
-  //     $.getJSON('https://viacep.com.br/ws/' + document.forms.cliente.cep.value + '/json/?callback=?', (data) => {
-  //       console.log(data);
-  //       if (data.erro === undefined) {
-  //         $(document.forms.cliente.estado).val(data.uf);
-  //         $(document.forms.cliente.cidade).val(data.localidade);
-  //         $(document.forms.cliente.bairro).val(data.bairro);
-  //         $(document.forms.cliente.rua).val(data.logradouro);
-  //       } else {
-  //         $('#cepInvalido').toggleClass('d-block');
-  //         $('#cepInvalido').toggleClass('d-none');
-  //       }
-  //     });
-  //   }
-  // });
+    $(document.forms.cliente.cep).on('focusout', (event) => {
+      if (event.target.validity.valid) {
+        $.getJSON('https://viacep.com.br/ws/' + document.forms.cliente.cep.value + '/json/?callback=?', (data) => {
+          console.log(data);
+          if (data.erro === undefined) {
+            $(document.forms.cliente.estado).val(data.uf);
+            $(document.forms.cliente.cidade).val(data.localidade);
+            $(document.forms.cliente.bairro).val(data.bairro);
+            $(document.forms.cliente.rua).val(data.logradouro);
+          } else {
+            $('#cepInvalido').toggleClass('d-block');
+            $('#cepInvalido').toggleClass('d-none');
+          }
+        });
+      }
+    });
+  }
 
-  $(document.forms.imovel.cep).on('focusin', (event) => {
-    $('#cepInvalido').removeClass('d-block');
-    $('#cepInvalido').addClass('d-none');
-  });
+  if (document.forms.imovel !== undefined) {
+    $(document.forms.imovel.cep).on('focusin', (event) => {
+      $('#cepInvalido').removeClass('d-block');
+      $('#cepInvalido').addClass('d-none');
+    });
 
-  $(document.forms.imovel.cep).on('focusout', (event) => {
-    if (event.target.validity.valid) {
-      $.getJSON('https://viacep.com.br/ws/' + document.forms.imovel.cep.value + '/json/?callback=?', (data) => {
-        console.log(data);
-        if (data.erro === undefined) {
-          $(document.forms.imovel.estado).val(data.uf);
-          $(document.forms.imovel.cidade).val(data.localidade);
-          $(document.forms.imovel.bairro).val(data.bairro);
-          $(document.forms.imovel.rua).val(data.logradouro);
-        } else {
-          $('#cepInvalido').toggleClass('d-block');
-          $('#cepInvalido').toggleClass('d-none');
-        }
-      });
-    }
-  });
+    $(document.forms.imovel.cep).on('focusout', (event) => {
+      if (event.target.validity.valid) {
+        $.getJSON('https://viacep.com.br/ws/' + document.forms.imovel.cep.value + '/json/?callback=?', (data) => {
+          console.log(data);
+          if (data.erro === undefined) {
+            $(document.forms.imovel.estado).val(data.uf);
+            $(document.forms.imovel.cidade).val(data.localidade);
+            $(document.forms.imovel.bairro).val(data.bairro);
+            $(document.forms.imovel.rua).val(data.logradouro);
+          } else {
+            $('#cepInvalido').toggleClass('d-block');
+            $('#cepInvalido').toggleClass('d-none');
+          }
+        });
+      }
+    });
 
-  $(document.forms.imovel.categoria).on('change', (event) => {
-    $('#cardApartamento').toggleClass('d-none');
-    $('#valorImovel').toggleClass('d-none');
-    $('#valorAluguel').toggleClass('d-none');
-  });
+    $(document.forms.imovel.categoria).on('change', (event) => {
+      $('#cardApartamento').toggleClass('d-none');
+      $('#valorImovel').toggleClass('d-none');
+      $('#valorAluguel').toggleClass('d-none');
+    });
 
-  let listaFotos = [];
+    let listaFotos = [];
 
-  $(document.forms.imovel.fotos).on('change', (event) => {
-    if (listaFotos.length < 6) {
-      listaFotos.push(document.forms.imovel.fotos.files[0]);
-      document.forms.imovel.numeroFotos.value = listaFotos.length;
+    $(document.forms.imovel.fotos).on('change', (event) => {
+      if (listaFotos.length < 6) {
+        listaFotos.push(document.forms.imovel.fotos.files[0]);
+        document.forms.imovel.numeroFotos.value = listaFotos.length;
 
-      let fileReader = new FileReader();
+        let fileReader = new FileReader();
 
-      fileReader.onload = function(e) {
-        let wrapper = $('<div class="col-md-4 py-2"></div>');
-        let img = $('<img class="gallery-image">');
-        img.attr('src', fileReader.result);
-        img.appendTo(wrapper);
-        wrapper.appendTo('#fotosImovel');
-      };
+        fileReader.onload = function(e) {
+          let wrapper = $('<div class="col-md-4 py-2"></div>');
+          let img = $('<img class="gallery-image">');
+          img.attr('src', fileReader.result);
+          img.appendTo(wrapper);
+          wrapper.appendTo('#fotosImovel');
+        };
 
-      fileReader.readAsDataURL(event.target.files[0]);
-    } else {
-      $('#fotosInvalido').addClass('d-block');
-      $('#fotosInvalido').removeClass('d-none');
-    }
-  });
+        fileReader.readAsDataURL(event.target.files[0]);
+      } else {
+        $('#fotosInvalido').addClass('d-block');
+        $('#fotosInvalido').removeClass('d-none');
+      }
+    });
+  }
 });
